bf98f27793bfffa6b686183b626aecd065123861
Magda Sz <pesahson@gmail.com>
2021-07-06 19:04:06 +0200

    add barba transitions

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ showBtn.addEventListener('click', () => {
 hideBtn.addEventListener('click', () => {
   mobileMenu.classList.remove('show-menu');
 })
+
+const allBandes = document.querySelectorAll(".bande");
+const TLAnim = new TimelineMax();
+
+function delay(n) {
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
+}
+
+barba.init({
+  sync: true,
+
+  transitions: [
+    {
+      async leave() {
+        const done = this.async();
+
+        TLAnim.to(allBandes, { width: "100%", stagger: 0.05 });
+
+        await delay(800);
+        done();
+      },
+      enter() {
+        TLAnim.to(allBandes, { width: "0%", stagger: 0.05 });
+      },
+    },
+  ],
+});
