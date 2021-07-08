e5726cb499257fee03077fcef7bccbf6927a415c
Magda Sz <pesahson@gmail.com>
2021-07-08 08:28:17 +0200

    gsap animation

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-const showBtn = document.querySelector('.show-menu');
-const mobileMenu = document.querySelector('.mobile-list');
-const hideBtn = document.querySelector('.hide-menu')
 
-showBtn.addEventListener('click', () => {
-  mobileMenu.classList.add('show-menu');
-})
 
-hideBtn.addEventListener('click', () => {
-  mobileMenu.classList.remove('show-menu');
-})
+const showBtn = document.querySelector(".show-menu");
+const mobileMenu = document.querySelector(".mobile-list");
+const hideBtn = document.querySelector(".hide-menu");
+
+showBtn.addEventListener("click", () => {
+  mobileMenu.classList.add("show-menu");
+});
+
+hideBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("show-menu");
+});
 
 const allBandes = document.querySelectorAll(".bande");
 const TLAnim = new TimelineMax();
 
+
+ 
 function delay(n) {
   return new Promise((done) => {
     setTimeout(() => {
@@ -37,6 +41,22 @@ barba.init({
       enter() {
         TLAnim.to(allBandes, { width: "0%", stagger: 0.05 });
       },
+      once() {
+         TLAnim.to(".anim-nav", { autoAlpha: 1, y: 0, stagger: 0.2 })
+        TLAnim.fromTo(
+          ".anim-header",
+          { autoAlpha: 0, x: 50, duration: 0.5 },
+          { autoAlpha: 1, x: 0 }
+        ).fromTo(
+          ".anim-text",
+          { autoAlpha: 0, x: -50, duration: 0.5 },
+          { autoAlpha: 1, x: 0 },
+          "-=.5"
+        );
+   
+
+      }
+     
     },
   ],
 });
