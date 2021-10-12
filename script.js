3d79ebe41f1f1e8c4da844057fc42c5a7b3a5139
Magda Sz <pesahson@gmail.com>
2021-10-12 14:03:26 +0200

    toggle working on the icon as well/not just the btn

--- script.js
+++ script.js
@@ -22,91 +22,88 @@ function delay(n) {
 }
 
 barba.hooks.afterEnter((data) => {
-       const dirCards = document.querySelectorAll(".directors-card");
-       const dirBtns = document.querySelectorAll(".directors-card-btn");
+  const dirCards = document.querySelectorAll(".directors-card");
+  const dirBtns = document.querySelectorAll(".directors-card-btn");
 
-       dirCards.forEach((card) => {
-         card.addEventListener("click", (e) => {
-           if (e.target.classList.contains("directors-card-btn")) {
-             console.log(card);
-             e.currentTarget.classList.toggle("flip");
-           }
-         });
-       });
+  dirCards.forEach((card) => {
+    card.addEventListener("click", (e) => {
+      if (e.target.classList.contains("toggle")) {
+        e.currentTarget.classList.toggle("flip");
+      }
+    });
+  });
 
-       const form = document.querySelector("form");
-       const userName = document.getElementById("name");
-       const title = document.getElementById("title");
-       const company = document.getElementById("company-name");
-       const text = document.getElementById("textarea");
-       const email = document.getElementById("email");
-       const allInputs = document.querySelectorAll(".unserInput");
+  // const form = document.querySelector("form");
+  // const userName = document.getElementById("name");
+  // const title = document.getElementById("title");
+  // const company = document.getElementById("company-name");
+  // const text = document.getElementById("textarea");
+  // const email = document.getElementById("email");
+  // const allInputs = document.querySelectorAll(".unserInput");
 
-       form.addEventListener("submit", (e) => {
-         e.preventDefault();
-         checkInputs();
-       });
+  // form.addEventListener("submit", (e) => {
+  //   e.preventDefault();
+  //   checkInputs();
+  // });
 
-      //  function isEmail(email) {
-      //    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-      //      email
-      //    );
-      //  }
+  //  function isEmail(email) {
+  //    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+  //      email
+  //    );
+  //  }
 
-       const checkInputs = () => {
-         const userNameValue = userName.value.trim();
-         const titleValue = title.value.trim();
-         const companyValue = company.value.trim();
-         const textValue = text.value.trim();
-         //const emailValue = email.value.trim();
+  const checkInputs = () => {
+    const userNameValue = userName.value.trim();
+    const titleValue = title.value.trim();
+    const companyValue = company.value.trim();
+    const textValue = text.value.trim();
+    //const emailValue = email.value.trim();
 
-         if (userNameValue === "") {
-           setError(userName, "Name field cannot be empty");
-         } else {
-           setSuccess(userName);
-         }
+    if (userNameValue === "") {
+      setError(userName, "Name field cannot be empty");
+    } else {
+      setSuccess(userName);
+    }
 
-         if (titleValue === "") {
-           setError(title, "Title field cannot be empty");
-         } else {
-           setSuccess(title);
-         }
+    if (titleValue === "") {
+      setError(title, "Title field cannot be empty");
+    } else {
+      setSuccess(title);
+    }
 
-         if (companyValue === "") {
-           setError(company, "Company field cannot be empty");
-         } else {
-           setSuccess(company);
-         }
+    if (companyValue === "") {
+      setError(company, "Company field cannot be empty");
+    } else {
+      setSuccess(company);
+    }
 
-         if (textValue === "") {
-           setError(text, "Message field cannot be empty");
-         } else {
-           setSuccess(text);
-         }
+    if (textValue === "") {
+      setError(text, "Message field cannot be empty");
+    } else {
+      setSuccess(text);
+    }
 
-        //  if (emailValue === "") {
-        //    setError(email, "Email field cannot be empty");
-        //  } else if (!isEmail(emailValue)) {
-        //    setError(email, "Please enter a valid email");
-        //  } else {
-        //    setSuccess(email);
-        //  }
-       };
+    //  if (emailValue === "") {
+    //    setError(email, "Email field cannot be empty");
+    //  } else if (!isEmail(emailValue)) {
+    //    setError(email, "Please enter a valid email");
+    //  } else {
+    //    setSuccess(email);
+    //  }
+  };
 
-       function setError(input, message) {
-         const inputWrapper = input.parentElement;
-         const small = inputWrapper.querySelector("small");
-         inputWrapper.className = "input-wrapper error";
-         small.innerText = message;
-       }
+  function setError(input, message) {
+    const inputWrapper = input.parentElement;
+    const small = inputWrapper.querySelector("small");
+    inputWrapper.className = "input-wrapper error";
+    small.innerText = message;
+  }
 
-       function setSuccess(input) {
-         const inputWrapper = input.parentElement;
-         inputWrapper.className = "input-wrapper success";
-       }
-
-})
-
+  function setSuccess(input) {
+    const inputWrapper = input.parentElement;
+    inputWrapper.className = "input-wrapper success";
+  }
+});
 
 barba.init({
   sync: true,
@@ -124,30 +121,25 @@ barba.init({
       enter() {
         window.scrollTo(0, 0);
         tl.to(allBandes, { scaleX: 0, stagger: 0.05 });
-     
       },
       once() {
-      tl.to(".styled-content", { autoAlpha: 1 })
-        .to(".anim-nav", { autoAlpha: 1, y: 0, stagger: 0.2 })
-        .fromTo(
-          ".anim-header",
-          { autoAlpha: 0, x: 50, duration: 0.5 },
-          { autoAlpha: 1, x: 0 }
-        )
-        .fromTo(
-          ".anim-text",
-          { autoAlpha: 0, x: -50, duration: 0.5 },
-          { autoAlpha: 1, x: 0 },
-          "<"
-        );
+        tl.to(".styled-content", { autoAlpha: 1 })
+          .to(".anim-nav", { autoAlpha: 1, y: 0, stagger: 0.2 })
+          .fromTo(
+            ".anim-header",
+            { autoAlpha: 0, x: 50, duration: 0.5 },
+            { autoAlpha: 1, x: 0 }
+          )
+          .fromTo(
+            ".anim-text",
+            { autoAlpha: 0, x: -50, duration: 0.5 },
+            { autoAlpha: 1, x: 0 },
+            "<"
+          );
       },
     },
   ],
 });
-
-
-
-
 
 // const form = document.querySelector('form');
 // const userName = document.getElementById('name');
@@ -157,12 +149,9 @@ barba.init({
 // const email = document.getElementById('email');
 // const allInputs = document.querySelectorAll('.unserInput');
 
-
-
 // form.addEventListener('submit', (e) => {
 //   e.preventDefault();
 // checkInputs();
-
 
 // })
 
@@ -224,9 +213,4 @@ barba.init({
 //   inputWrapper.className = "input-wrapper success";
 // }
 
-
 // FLIPPING CARDS
-
-
-
-
